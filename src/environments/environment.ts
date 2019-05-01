9087e91afd272c3fe4b4eb95de0b0cb078ac3f4d
matthew-trump <matthew.trump@gmail.com>
2019-05-01 15:06:06 -0700

    configs imported from quizzes project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,38 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    useWelcomeStages: true,
+    animations: {
+      welcome: {
+        titles: {
+          logoEnter: { initialVerticalOffset: '200px', duration: '700ms', delay: '0ms' },
+          descriptionEnter: { initialVerticalOffset: '200px', duration: '700ms', delay: '200ms' },
+          leave: { duration: '400ms' }
+        },
+        instructions: {
+          enter: [
+            { initialVerticalOffset: '50px', duration: '400ms', delay: '0ms' },
+            { initialVerticalOffset: '50px', duration: '400ms', delay: '200ms' },
+            { initialVerticalOffset: '50px', duration: '400ms', delay: '400ms' }
+          ],
+          leave: { scaling: '1.5', duration: '400ms' }
+        }
+      }
+    },
+    background: {
+      color: '#FFFFFF',
+      circularWipe: {
+        welcome: {
+          initialBackgroundColor: '#FFFFFF',
+          finalBackgroundColor: '#dddddd',
+          durationInMilliseconds: 1300
+        },
+      }
+    }
+  }
+
 };
 
 /*
