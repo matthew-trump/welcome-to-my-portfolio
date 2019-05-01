ce6c20fd1e0404622a53bdf2c68ff67704d89058
matthew-trump <matthew.trump@gmail.com>
2019-05-01 15:05:42 -0700

    imported from quizzes project to provide animation stages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-portfolio';
+
+  view: string = "welcome";
+
+  constructor() { }
+  ngOnInit() { }
+
 }
