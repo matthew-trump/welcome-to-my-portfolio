0aa7592b960dc4e341c98325f75411b6007ac640
matthew-trump <matthew.trump@gmail.com>
2019-05-01 15:05:55 -0700

    component declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundEffectComponent } from './background-effect/background-effect.component';
+import { StyledCircleWipeComponent } from './styled-circle-wipe/styled-circle-wipe.component';
+import { StyledBackgroundComponent } from './styled-background/styled-background.component';
+
+import { BackgroundReducer } from './reducers';
+import { WelcomeViewComponent } from './welcome-view/welcome-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundEffectComponent,
+    StyledCircleWipeComponent,
+    StyledBackgroundComponent,
+    WelcomeViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      background: BackgroundReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
